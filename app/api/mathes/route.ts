6b085ade10debefbd8bca5ee1b47a899f815d914
jahanb <jahanb@gmail.com>
2025-12-08 00:36:37 +0100

    delete a folder

--- app/api/mathes/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-import { connectDB } from "@/lib/db";
-import Match from "@/models/Match";
-
-export async function GET() {
-    await connectDB();
-    const matches = await Match.find({}).sort({ startTime: 1 });
-    return NextResponse.json(matches);
-}
